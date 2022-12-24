3058a6ae967097a3a4fec6cb8824c56e9687dbad
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-24 19:58:24 -0300

    feat: add mymap function to array prototype

--- src/index.js
+++ src/index.js
@@ -13,3 +13,24 @@ const myMap = (array, fn) => {
 
 const myMappedArray = myMap(array, n => n * 2);
 console.log('my map', myMappedArray);
+
+function myPrototypeMap(fn) {
+  const newArray = [];
+  for (let i = 0; i < this.length; i++) {
+    newArray.push(fn(this[i]));
+  }
+  return newArray;
+}
+
+// Array.prototype.myMap = myPrototypeMap;
+// Object.assign(Array.prototype, { myMap: myPrototypeMap });
+Object.defineProperty(Array.prototype, 'myMap', {
+  value: myPrototypeMap,
+  writable: true,
+  enumerable: false,
+  configurable: true
+});
+
+const myPrototypeMappedArray = array.myMap(n => n * 2);
+console.log('my prototype map', myPrototypeMappedArray);
+
