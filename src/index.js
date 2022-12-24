b1bad1212e903a123ad9409bba07ce36f560c574
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-24 20:26:18 -0300

    refact: object shorthand

--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ const array = [1, 2, 3, 4, 5];
 //  enumerable: false,
 //  configurable: true
 // });
-Object.assign(Array.prototype, { myMap: myMap });
+Object.assign(Array.prototype, { myMap });
 const myPrototypeMappedArray = array.myMap(n => n * 2);
 console.log('my prototype map', myPrototypeMappedArray);
 
-Object.assign(Array.prototype, { myFilter: myFilter });
+Object.assign(Array.prototype, { myFilter });
 const myPrototypeFilteredArray = array.myFilter(n => n % 2 === 0);
 console.log('my prototype filter', myPrototypeFilteredArray);
 
