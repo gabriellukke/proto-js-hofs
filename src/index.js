bbdf1bcc2703ecc78b60138768bc5fcf5a8ab0f9
Gabriel Almeida <gabriel.dev.almeida@outlook.com>
2022-12-24 19:54:02 -0300

    feat: my map

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
-console.log('start');
+const array = [1, 2, 3, 4, 5];
 
+const mappedArray = array.map(n => n * 2);
+console.log('js map', mappedArray); // [2, 4, 6, 8, 10]
+
+const myMap = (array, fn) => {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(fn(array[i]));
+  }
+  return newArray;
+}
+
+const myMappedArray = myMap(array, n => n * 2);
+console.log('my map', myMappedArray);
